aca2faa59a7edf62d02404a3215d6a09be6203fc
Muhammad Shoaib Riaz <shoaib.riaz@arbisoft.com>
2019-08-20 13:51:52 +0500

    Updates

---
+++ precache-manifest.330eca1b4551e1ec9a7d92dfa773e08a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2f9dbca01b06474d849d78afc2541e",
+    "url": "/ShoaibKhazer/ReactDashboard/index.html"
+  },
+  {
+    "revision": "5bf3413fc316820b044b",
+    "url": "/ShoaibKhazer/ReactDashboard/static/css/2.46ec7e98.chunk.css"
+  },
+  {
+    "revision": "12181e36f33fd5262c8f",
+    "url": "/ShoaibKhazer/ReactDashboard/static/css/main.3605a03f.chunk.css"
+  },
+  {
+    "revision": "5bf3413fc316820b044b",
+    "url": "/ShoaibKhazer/ReactDashboard/static/js/2.2e3c88c4.chunk.js"
+  },
+  {
+    "revision": "12181e36f33fd5262c8f",
+    "url": "/ShoaibKhazer/ReactDashboard/static/js/main.64a3db83.chunk.js"
+  },
+  {
+    "revision": "38f2e1afb74f3541b4c0",
+    "url": "/ShoaibKhazer/ReactDashboard/static/js/runtime~main.94a6b730.js"
+  },
+  {
+    "revision": "023ba0824a99109c4a63622228dd6354",
+    "url": "/ShoaibKhazer/ReactDashboard/static/media/feather.023ba082.svg"
+  },
+  {
+    "revision": "5fad700adc948cb51404d55833347f51",
+    "url": "/ShoaibKhazer/ReactDashboard/static/media/feather.5fad700a.eot"
+  },
+  {
+    "revision": "66cbb621b431bf32041a5c478e5539c0",
+    "url": "/ShoaibKhazer/ReactDashboard/static/media/feather.66cbb621.woff"
+  },
+  {
+    "revision": "a940fe89dbfe9d1d89fc1aa0488fe032",
+    "url": "/ShoaibKhazer/ReactDashboard/static/media/feather.a940fe89.ttf"
+  },
+  {
+    "revision": "089843331d9c99fcf92046e6006a64c3",
+    "url": "/ShoaibKhazer/ReactDashboard/static/media/pct.08984333.ttf"
+  },
+  {
+    "revision": "1313271c94bb80d280402c07813bac9c",
+    "url": "/ShoaibKhazer/ReactDashboard/static/media/pct.1313271c.svg"
+  },
+  {
+    "revision": "7df89a766a56eb2fd582fbf742ec8971",
+    "url": "/ShoaibKhazer/ReactDashboard/static/media/pct.7df89a76.woff"
+  },
+  {
+    "revision": "9ee6254785f847481a4c568f6d44a01a",
+    "url": "/ShoaibKhazer/ReactDashboard/static/media/pct.9ee62547.eot"
+  }
+]);
